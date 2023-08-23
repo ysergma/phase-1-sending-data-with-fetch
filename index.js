860657dfe76d03a28f8fd62857ad9aa004d065e0
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-08-23 20:21:00 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Add your code here
+const BASE_URL="http://localhost:3000";
+function submitData(name,email) {
+    return fetch("http://localhost:3000/users",{
+        method : "POST",
+        headers: {
+            'Content-Type':'application/json',
+            'Accept':'application/json',
+         },
+         body:JSON.stringify( {
+            name : `${name}`,
+            email: `${email}`,
+
+        }),
+         
+    })
+    
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        const id =response.id
+        const myd = document.querySelector(".id")
+       const div = document.createElement('div');
+      // document.textContent = response.id
+      console.log(response);
+        div.textContent = id
+        ///
+       // console.log(div);
+     myd.append(div)
+      })
+      .catch(function(error){
+        const message = error.message
+        const myd = document.querySelector(".id")
+        const div = document.createElement('div');
+       // document.textContent = response.id
+       console.log(error);
+         div.textContent = message
+         ///
+        // console.log(div);
+      myd.append(div)
+      })
+
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded",submitData)
